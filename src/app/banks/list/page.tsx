7336fb97796fa26b9a18562908396e2023b5d9ac
Alirayyan0011 <ali.rayyan001@gmail.com>
2025-01-21 01:24:19 +0500

    Banks Table

---
+++ src/app/banks/list/page.tsx
@@ -0,0 +1,11 @@
+import BankList from "@/components/bank/banklist/list";
+import NavbarComponent from "@/components/navbar/navbar";
+
+export default function BankListPage() {
+    return (
+        <div>
+            <NavbarComponent />
+            <BankList />
+        </div>
+    );
+}
